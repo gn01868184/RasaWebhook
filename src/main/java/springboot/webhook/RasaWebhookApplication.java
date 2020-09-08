d647504842d0ebebd0197de25cf765a85dcc330b
gn01868184 <gn01868184@yahoo.com.tw>
2020-09-08 09:41:54 +0800

    Create RasaWebhookApplication.java

---
+++ src/main/java/springboot/webhook/RasaWebhookApplication.java
@@ -0,0 +1,13 @@
+package springboot.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasaWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasaWebhookApplication.class, args);
+	}
+
+}
